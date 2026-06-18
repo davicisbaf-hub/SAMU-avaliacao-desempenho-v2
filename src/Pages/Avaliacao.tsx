bddb0d69966fe3ae67249387b067a61e8ba41ce0
Davi Cisbaf <davi.cisbaf@gmail.com>
2026-06-18 15:25:43 -0300

    servidor

--- src/Pages/Avaliacao.tsx
+++ src/Pages/Avaliacao.tsx
@@ -99,7 +99,7 @@ export default function AvaliacaoPage() {
 
 		try {
 			const response = await fetch(
-				"http://localhost:3001/api/avaliacoes",
+				"http://192.168.1.10:8026/api/avaliacoes",
 				{
 					method: "POST",
 					headers: {
@@ -129,7 +129,7 @@ export default function AvaliacaoPage() {
 
 	useEffect(() => {
 		async function carregarBases() {
-			const res = await fetch("http://localhost:3001/api/bases"); // sua rota backend
+			const res = await fetch("http://192.168.1.10:8026/api/bases"); // sua rota backend
 			const data = await res.json();
 
 			setBases(data);
@@ -139,11 +139,11 @@ export default function AvaliacaoPage() {
 
 
 	useEffect(() => {
-		fetch("http://localhost:3001/api/escala-likert")
+		fetch("http://192.168.1.10:8026/api/escala-likert")
 			.then((r) => r.json())
 			.then(setEscalaLikert);
 
-		fetch("http://localhost:3001/api/pesos-avaliacao")
+		fetch("http://192.168.1.10:8026/api/pesos-avaliacao")
 			.then((r) => r.json())
 			.then(setPesos);
 	}, []);
@@ -171,7 +171,7 @@ export default function AvaliacaoPage() {
 
 	useEffect(() => {
 		carregar(
-			`http://localhost:3001/api/criterios-avaliacao-autoavaliacao/${tipoAvaliacao}`,
+			`http://192.168.1.10:8026/api/criterios-avaliacao-autoavaliacao/${tipoAvaliacao}`,
 			setCriterios
 		);
 	}, [tipoAvaliacao]);
